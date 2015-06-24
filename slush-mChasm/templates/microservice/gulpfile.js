0a1c1bae86e09b8e2b11a5210d3306452f6d7390
Shawn Kircher <codeshaghennessy@gmail.com>
2015-06-24 16:51:01 -0500

    Cleanup

--- slush-mChasm/templates/microservice/gulpfile.js
+++ slush-mChasm/templates/microservice/gulpfile.js
@@ -5,9 +5,6 @@ process.on('uncaughtException', function (err) {
     process.exit(1);
 });
 
-//var stackPath = require('path').join(__dirname, '../')
-//require('app-module-path').addPath(stackPath);
-
 var microConfig = {
     name: '<%=chasmNameSlug%>-<%=serviceNameSlug%>',
     desc: '<%=chasmNameSlug%> <%=serviceNameSlug%> microservice',
@@ -23,180 +20,17 @@ var microConfig = {
     }
 };
 
-//var microservice = stackRequire('eb-microsvc')(microConfig);
-//var microservice = require('eb-microsvc')(microConfig);
-//Logger.debug(microservice);
-//
-//var microGulpTasksDir = '../gulp-tasks/';
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
-//var plugins = require('gulp-load-plugins')({
-//    //pattern: ['gulp-*', 'gulp.*'], // the glob(s) to search for
-//    pattern: ['*'], // the glob(s) to search for
-//    //config: require('path').join(__dirname, 'package.json'),
-//    config: require('path').join(__dirname + '/' + microGulpTasksDir, 'package.json'),
-//    scope: ['devDependencies'],  // which keys in the config to look within
-//    replaceString: /^gulp(-|\.)/, // what to remove from the name of the module when adding it to the context
-//    camelize: true, // if true, transforms hyphenated plugins names to camel case
-//    lazy: false, // whether the plugins should be lazy loaded on demand
-//    rename: {"aws-sdk": "awsSdk"} // a mapping of plugins to rename
-//});
-//
-//function getTask(task, options) {
-//    if (typeof options !== 'object') {
-//        options = {};
-//    }
-//    return require(microGulpTasksDir + task)(gulp, plugins, options, __dirname);
-//}
-
-//var archiveName = function (extension) {
-//    return microservice.definition.elasticbeanstalk.ebAppName + '_v' + packageFileVersion() + extension;
-//};
-
-/**
- * Dynamically reload the version number from the package.json incase the version has been bumped since it was loaded
- * @returns version number from package.json
- */
-var packageFileVersion = function () {
-    delete require.cache[require.resolve('./package.json')]; //uncache the module
-    return require('./package.json').version;
-};
-
-//var privates = ['eb-microsvc', 'topology'];
-//
-//var localModuleCache = 'local_module_cache';
-
-//gulp.task('clean-stack-cache', getTask('clean-local-cache', {dir: localModuleCache}, function (done) {
-//}));
-//gulp.task('clean-pm-cache', getTask('clean-pm-cache', {dir: localModuleCache}, function (done) {
-//}));
-
-//gulp.task('get-stack-modules', function (done) {
-//
-//    var clone_stack_modules = function (module, done) {
-//        plugins.util.log(plugins.util.colors.green.bgBlack('[GULP] clone module ', module));
-//        plugins.mkdirp(localModuleCache, function (err) {
-//            if (err) plugins.util.log(plugins.util.colors.red.bgWhite('[GULP]: error ', err));
-//        });
-//        var modbase='../'+module;
-//        return gulp.src([modbase +'/**/*.*', '!'+modbase+'/node_modules/**']).pipe(gulp.dest('./' +localModuleCache +'/'+ module)).pipe(plugins.debug()).on('finish', function () {
-//            plugins.util.log(plugins.util.colors.red.bgBlack('[GULP]: copy finished'));
-//        });
-//    };
-//    plugins.async.each(privates, clone_stack_modules, function () {
-//        done();
-//
-//    });
-//});
-
-//gulp.task('roll-minor-version', getTask('roll-version', {segment: 'minor'}, function (done) {
-//}));
-
-//var releaseDir = "_deploy_" + packageFileVersion();
-//
-//var stage_sources = [
-//    './.dockerignore',
-//    './Dockerfile',
-//    './apis/**/*.*',
-//    './config/**/*.*',
-//    './lib/**/*.*',
-//    './logger.js',
-//    './manifest.js',
-//    './package.json',
-//    './server.js',
-//    localModuleCache + '/**/*.*'
-//];
-
-//gulp.task('stage-files', getTask('stage-release', {
-//    sources: stage_sources,
-//    releaseDir: releaseDir
-//}, function (done) {
-//    plugins.util.log(plugins.util.colors.green.bgBlack('[GULP] stage-release done'));
-//    done();
-//}));
-
-//gulp.task('stage-npm', getTask('stage-npm-install', {
-//    releaseDir: releaseDir,
-//    pmCache: localModuleCache
-//}, function (done) {
-//    done();
-//}));
-
-//
-//gulp.task('archive-release', getTask('archive-release', {
-//    releaseDir: releaseDir,
-//    archiveName: archiveName('.zip')
-//}, function (done) {
-//    done();
-//}));
-//
-//gulp.task('clean-releases', getTask('clean-release-archives', {sources: ['_deploy*', '*.zip']}, function (done) {
-//    done();
-//}));
-//
-//gulp.task('roll-patch-version', getTask('roll-version', {segment: 'patch'}, function (done) {
-//}));
-//
-//gulp.task('s3-upload', getTask('s3-upload-version', {
-//    bucket: microservice.definition.s3.bucket,
-//    archiveName: archiveName('.zip')
-//}, function (done) {
-//    done();
-//}));
-//
-//gulp.task('eb-version', getTask('eb-app-version', {
-//    awsProfile: microservice.definition.awsCredentialsProfile,
-//    region: microservice.definition.region,
-//    bucket: microservice.definition.s3.bucket,
-//    archiveName: archiveName('.zip'),
-//    appName: microservice.definition.elasticbeanstalk.ebAppName,
-//    version: packageFileVersion()
-//}, function (done) {
-//    done();
-//}));
-//
-//gulp.task('eb-load', getTask('eb-load-version', {
-//    awsProfile: microservice.definition.awsCredentialsProfile,
-//    region: microservice.definition.region,
-//    ebEnvironment: microservice.definition.elasticbeanstalk.ebEnvName,
-//    version: packageFileVersion()
-//}, function (done) {
-//    done();
-//}));
-
-/****************************************
- * Consolidation tasks
- ****************************************/
-//gulp.task('eb-deploy', function (done) {
-//    runSequence('s3-upload', 'eb-version', 'eb-load');
-//});
-//gulp.task('full-stage', function (done) {
-//    runSequence('build', 'stage-files',  'archive-release');
-//    done();
-//});
 
 gulp.task('build', function (done) {
-    Logger.debug('[GULP] build stub');
+    Logger.debug('[GULP] build');
     done();
 });
 
-//gulp.task('test', function () {
-//    plugins.nodemon({
-//        script: 'server.js'
-//        , env: {'NODE_ENV': 'test'}
-//    });
-//});
-//
-//gulp.task('debug', function () {
-//    plugins.nodemon({
-//        script: 'server.js'
-//        , env: {'NODE_ENV': 'local_docker'}
-//    });
-//});
-
 gulp.task('default', function (done) {
     runSequence('build');
-    Logger.debug('[GULP] default continue');
+    Logger.debug('[GULP] default');
     done();
 });
 
